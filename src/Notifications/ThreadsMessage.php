77de479ac250d583b376776584aadc65a2b15ab5
kawax <kawaxbiz@gmail.com>
2024-09-16 17:33:00 +0900

    Update ThreadsMessage.php

--- src/Notifications/ThreadsMessage.php
+++ src/Notifications/ThreadsMessage.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace Revolution\Threads\Notifications;
 
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
 
-class ThreadsMessage implements Arrayable
+final class ThreadsMessage implements Arrayable
 {
+    use Conditionable;
+    use Macroable;
+
     public ?string $image_url = null;
     public ?string $video_url = null;
     public int $sleep = 0;
@@ -17,19 +22,19 @@ class ThreadsMessage implements Arrayable
     ) {
     }
 
-    public static function create(string $text): static
+    public static function create(string $text): self
     {
-        return new static(text: $text);
+        return new self(text: $text);
     }
 
-    public function withImage(string $url): static
+    public function withImage(string $url): self
     {
         $this->image_url = $url;
 
         return $this;
     }
 
-    public function withVideo(string $url, int $sleep = 30): static
+    public function withVideo(string $url, int $sleep = 30): self
     {
         $this->video_url = $url;
         $this->sleep = $sleep;
