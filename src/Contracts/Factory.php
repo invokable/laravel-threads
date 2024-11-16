4d6e237e3410beab7bd08d4d84f19a907e6dc22d
kawax <kawaxbiz@gmail.com>
2024-11-16 20:56:42 +0900

    Update Factory.php

--- src/Contracts/Factory.php
+++ src/Contracts/Factory.php
@@ -52,21 +52,21 @@ interface Factory
      *
      * @return Response{id: string} Threads Media Container ID
      */
-    public function createText(string $text, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $link_attachment = null, ?string $quote_post_id = null): Response;
+    public function createText(string $text, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $link_attachment = null, ?string $quote_post_id = null, ?array $options = []): Response;
 
     /**
      * Create Image Container.
      *
      * @return Response{id: string} Threads Media Container ID
      */
-    public function createImage(string $url, ?string $text = null, bool $is_carousel = false, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $alt_text = null, ?string $quote_post_id = null): Response;
+    public function createImage(string $url, ?string $text = null, bool $is_carousel = false, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $alt_text = null, ?string $quote_post_id = null, ?array $options = []): Response;
 
     /**
      * Create Video Container.
      *
      * @return Response{id: string} Threads Media Container ID
      */
-    public function createVideo(string $url, ?string $text = null, bool $is_carousel = false, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $alt_text = null, ?string $quote_post_id = null): Response;
+    public function createVideo(string $url, ?string $text = null, bool $is_carousel = false, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $alt_text = null, ?string $quote_post_id = null, ?array $options = []): Response;
 
     /**
      * Create Carousel Container.
@@ -74,7 +74,7 @@ interface Factory
      * @param  array  $children  Container IDs
      * @return Response{id: string} Threads Media Container ID
      */
-    public function createCarousel(array $children, ?string $text = null, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $quote_post_id = null): Response;
+    public function createCarousel(array $children, ?string $text = null, ?ReplyControl $reply_control = null, ?string $reply_to_id = null, ?string $quote_post_id = null, ?array $options = []): Response;
 
     /**
      * Repost.
